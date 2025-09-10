c1f35b14d76de993dbdf091aee6e4431fa47668e
lavshtyagi <lavishtyagi2003@gmail.com>
2025-09-10 13:16:07 +0530

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,16 @@
+// console.log(2>1);
+
+// console.log(2!=1);
+
+
+
+// console.log("2" > 1);
+
+// console.log("02" > 1);
+
+
+// console.log( null > 1);
+
+// strick check ===
+console.log("2" === 2);
+
