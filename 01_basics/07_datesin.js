a7d3f6a1df16524946c1995e72e674cb935689f2
lavshtyagi <lavishtyagi2003@gmail.com>
2025-09-10 16:34:48 +0530

    dates&time

---
+++ 01_basics/07_datesin.js
@@ -0,0 +1,18 @@
+//dates
+
+let mydate = new Date()
+
+console.log(mydate.toDateString());
+
+console.log(mydate.toISOString());
+
+console.log(mydate.toJSON());
+
+console.log(mydate.toLocaleDateString());
+
+console.log(mydate.toLocaleTimeString());
+
+console.log(typeof mydate.toLocaleDateString());
+
+
+
