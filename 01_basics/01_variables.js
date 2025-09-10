c4f97b21aa4036273a281a162aa3d81de29e1854
lavshtyagi <lavishtyagi2003@gmail.com>
2025-09-10 12:05:46 +0530

    bsics

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -2,3 +2,23 @@ const account_Id = 144553
 let accountemaol = "lavishtyagi2003@gmail.com"
 var accountpassword = "12345"
 accountcity = "pune"
+
+// account_Id = 2 not allowed
+
+accountemaol = "hch@has.com"
+accountpassword="12122"
+accountcity="jaipur"
+
+let accountstate;
+
+console.log(account_Id);
+
+/*
+
+prefer not to use var
+
+becouse of issue in block scope and functional scope
+
+*/
+
+console.table([account_Id,accountemaol,accountpassword , accountcity ,accountstate])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,29 @@
+"use strict"; //treat all js code as newer version
+
+// alert(3+3)
+
+console.log(3+3) 
+
+console.log("lavish")
+
+let name = "Lavish"
+
+let age = 18
+
+let isloggedin = false
+
+// number=>2 to power 53
+// BigInt
+// String
+// Boolean
+// null
+// undefined
+// Symbol =>uniqness
+
+
+// Object
+
+
+console.log(typeof undefined);
+
+console.log(typeof null)
