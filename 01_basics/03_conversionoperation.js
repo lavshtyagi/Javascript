37e48ae9369a056c7d97eed6e98199e8c086dc27
lavshtyagi <lavishtyagi2003@gmail.com>
2025-09-10 12:35:45 +0530

    conversion

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,32 @@
+let score = true
+
+
+// console.log(typeof score);
+
+// console.log(typeof (score));
+
+// let valueinnumber = Number(score);
+
+// console.log(typeof valueinnumber);
+// console.log(valueinnumber);
+
+// 33 => 33
+// "33abs0" => NaN
+
+let isloggedin = "rude"
+
+let booleanIsloggedIn = Boolean(isloggedin)
+//console.log(booleanIsloggedIn);
+
+// 1 = true , 0 => false
+// "" => false
+// "string" => true
+
+let num = 33
+
+let string = String(num);
+
+console.log(string);
+
+console.log(typeof string);
+
