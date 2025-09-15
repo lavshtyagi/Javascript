964e3b9c27a77cf6db744d54ebd79f8ccd6b16cd
lavshtyagi <lavishtyagi2003@gmail.com>
2025-09-15 15:46:17 +0530

    loop

---
+++ 04_control_flow/two.js
@@ -0,0 +1,42 @@
+
+const greetings = "hello world"
+
+const word = greetings.split(" ");
+
+for(const char of word){
+    // console.log(`split word is ${char}`)
+}
+
+const myobject = {
+    js : 'javascript',
+
+    cpp: 'c++',
+
+    rb : 'ruby',
+
+    ts: 'typescript'
+}
+
+for(const key in myobject){
+    // console.log(`${key} shortcut is for ${myobject[key]}`);
+}
+
+const mycoding = [
+    {
+        languagename:"javascript",
+        languagefilename:'js'
+    },
+    {
+        languagename:"c++",
+        languagefilename:'cpp'
+    },
+    {
+        languagename:"typescript",
+        languagefilename:'ts'
+    }
+]
+
+mycoding.forEach((name) => {
+
+    console.log(name.languagename);
+})
